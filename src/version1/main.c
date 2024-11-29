7cd7bd9c4b09f4b9df8fff68ac4c4f84ff179ec6
jho7535 <jho7535@naver.com>
2024-11-29 14:19:22 +0900

    change host, port

--- src/version1/main.c
+++ src/version1/main.c
@@ -6,9 +6,9 @@ int main()
 {
     int target_port = 39071; // 프록시 서버 포트
     // char *target_host = "10.198.138.212"; // 프록시 서버 주소(swist1)
-    // char *target_host = "10.198.138.213"; // 프록시 서버 주소(swist2)
+    char *target_host = "10.198.138.213"; // 프록시 서버 주소(swist2)
 
-    char *target_host = "127.0.0.1"; // 로컬 테스트용
+    // char *target_host = "127.0.0.1"; // 로컬 테스트용
 
     return run_client(target_port, target_host);
 }
